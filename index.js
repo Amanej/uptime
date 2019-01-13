400966ee67b91199eac014543f3e058a1eea2aee
Aman <aman.mender@tegningtil3d.no>
2019-01-13 02:26:33 +0100

    Added payload parsing

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Main file
 const http = require('http');
 const url = require('url');
-
+const StringDecoder = require('string_decoder').StringDecoder;
 
 // Server setup
 const server  = http.createServer((req,res) => {
@@ -15,12 +15,24 @@ const server  = http.createServer((req,res) => {
     let method = req.method.toLowerCase();
     // Get headers as an object
     const headers = req.headers;
-    // Send response
-    res.end(`Hello Aman\n ${trimmedPath}`);
-    // Log
-    console.log('Request received on path: '+trimmedPath+' with method '+method);
-    console.log(`With these query string parameters `,queryStringObject);
-    console.log('Here is the headers ',headers);
+    // Parse Payload, if any
+    let decoder = new StringDecoder('utf-8');
+    let buffer = '';
+    req.on('data',(data) => {
+        buffer += decoder.write(data)
+    });
+    req.on('end', () => {
+        buffer += decoder.end();
+
+        // When the buffer has ended, we will do something ie respond and log
+        // Send response
+        res.end(`Hello Aman\n ${trimmedPath}`);
+        // Log
+        console.log('Request received on path: '+trimmedPath+' with method '+method);
+        console.log(`With these query string parameters `,queryStringObject);
+        console.log('Here is the headers ',headers);
+        console.log('Buffer ',buffer);
+    })
 })
 
 server.listen('2019',function() {
