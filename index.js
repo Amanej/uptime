138e750bb28cbacd0629e6518068c528450c5479
Aman <aman.mender@tegningtil3d.no>
2019-01-13 02:04:02 +0100

    added logging of method

--- index.js
+++ index.js
@@ -9,10 +9,12 @@ const server  = http.createServer((req,res) => {
     let parsedUrl = url.parse(req.url,true);
     // Get path
     let trimmedPath = parsedUrl.pathname.replace(/^\/+|\/+$/g,'');
+    // Get HTTP Method
+    let method = req.method.toLowerCase();
     // Send response
     res.end(`Hello Aman\n ${trimmedPath}`);
-
     // Log
+    console.log('Request received on path: '+trimmedPath+' with method '+method)
 })
 
 server.listen('2019',function() {
