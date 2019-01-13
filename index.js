9a5402b953ae52714cac0f11cec0393728c36261
Aman <aman.mender@tegningtil3d.no>
2019-01-13 02:08:59 +0100

    Parsed query strings

--- index.js
+++ index.js
@@ -6,15 +6,21 @@ const url = require('url');
 // Server setup
 const server  = http.createServer((req,res) => {
     // Parse url 
-    let parsedUrl = url.parse(req.url,true);
+    const parsedUrl = url.parse(req.url,true);
     // Get path
     let trimmedPath = parsedUrl.pathname.replace(/^\/+|\/+$/g,'');
+    // Get query string as an object
+    const queryStringObject = parsedUrl.query;
     // Get HTTP Method
     let method = req.method.toLowerCase();
+    // Get headers as an object
+    const headers = req.headers;
     // Send response
     res.end(`Hello Aman\n ${trimmedPath}`);
     // Log
-    console.log('Request received on path: '+trimmedPath+' with method '+method)
+    console.log('Request received on path: '+trimmedPath+' with method '+method);
+    console.log(`With these query string parameters `,queryStringObject);
+    console.log('Here is the headers ',headers);
 })
 
 server.listen('2019',function() {
